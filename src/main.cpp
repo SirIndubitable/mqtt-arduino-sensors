26b394b6764c9587df741cbec53c880b4dc4ed19
Matt Olson <SirIndubitable@gmail.com>
2021-09-06 18:47:25 -0500

    Get MQTT actually working

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <Client.h>
 #include <SPI.h>
-#include <WiFi101.h>
 #include <CooperativeMultitasking.h>
-#include <MQTTClient.h>
+#include <PubSubClient.h>
 #include "secrets.h"
+#include <WiFi.h>
 
 #define SECONDS 1000
 
-char topicname[] = "arduino-test";
+char topicname[] = "testtopic/2";
 
 CooperativeMultitasking tasks;
 Continuation beginWiFiIfNeeded;
@@ -16,9 +16,22 @@ Continuation connectMQTTClientIfNeeded;
 Continuation on;
 Continuation off;
 
-WiFiClient wificlient;
-MQTTClient mqttclient(&tasks, &wificlient, MQTT_HOST, 1883, MQTT_CLIENT, MQTT_USERNAME, MQTT_PASSWORD);
-MQTTTopic topic(&mqttclient, topicname);
+WiFiSSLClient wificlient;
+PubSubClient mqttclient(wificlient);
+
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (unsigned int i=0; i<length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+}
+
 
 void setup()
 {
@@ -29,13 +42,12 @@ void setup()
         delay(1 * SECONDS);
     }
 
-    Serial.println("begin wifi");
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    tasks.after(10 * SECONDS, beginWiFiIfNeeded);
+    WiFi.setHostname("MyArduino");
+    beginWiFiIfNeeded();
 
-    Serial.println("connect mqtt client");
-    mqttclient.connect();
-    tasks.after(15 * SECONDS, connectMQTTClientIfNeeded);
+    mqttclient.setServer(MQTT_HOST, MQTT_PORT);
+    mqttclient.setCallback(callback);
+    connectMQTTClientIfNeeded();
 
     pinMode(LED_BUILTIN, OUTPUT);
     tasks.now(on);
@@ -44,19 +56,28 @@ void setup()
 void loop()
 {
     tasks.run();
+    mqttclient.loop();
 }
 
 void on()
 {
-    topic.publish("\"on\"");
     digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("on");
+    if (mqttclient.connected())
+    {
+        mqttclient.publish(topicname, "\"on\"");
+    }
     tasks.after(5 * SECONDS, off);
 }
 
 void off()
 {
-    topic.publish("\"off\"");
     digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("off");
+    if (mqttclient.connected())
+    {
+        mqttclient.publish(topicname, "\"off\"");
+    }
     tasks.after(5 * SECONDS, on);
 }
 
@@ -64,13 +85,16 @@ void beginWiFiIfNeeded()
 {
     switch (WiFi.status())
     {
-        case WL_IDLE_STATUS:
         case WL_CONNECTED:
+            Serial.println("Wifi connected");
             tasks.after(30 * SECONDS, beginWiFiIfNeeded);
             return;
         case WL_NO_SHIELD:
             Serial.println("no wifi shield");
             return;
+        case WL_IDLE_STATUS:
+            Serial.println("wifi idle");
+            break;
         case WL_CONNECT_FAILED:
             Serial.println("wifi connect failed");
             break;
@@ -88,11 +112,21 @@ void beginWiFiIfNeeded()
 
 void connectMQTTClientIfNeeded()
 {
-    if (!mqttclient.connected())
+    if (WiFi.status() != WL_CONNECTED)
     {
-        Serial.println("mqtt client not connected");
-        mqttclient.connect();
+        // If we have no wifi, we can't connect to MQTT, so wait a second
+        tasks.after(1 * SECONDS, connectMQTTClientIfNeeded);
+        return;
     }
 
-    tasks.after(30 * SECONDS, connectMQTTClientIfNeeded);
+    if (mqttclient.connected() || mqttclient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD))
+    {
+        tasks.after(30 * SECONDS, connectMQTTClientIfNeeded);
+        return;
+    }
+
+    Serial.print("mqtt failed, rc=");
+    Serial.print(mqttclient.state());
+    Serial.println(" try again in 5 seconds");
+    tasks.after(5 * SECONDS, connectMQTTClientIfNeeded);
 }
